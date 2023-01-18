f4a0a1dbc777e9eae1900b630ef0dba6d54066d0
Duran11 <antonio11duran@gmail.com>
2023-01-18 09:56:08 -0700

    contact form completed with bootstrap

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <div>
       <h2>About Me</h2>
+      <img></img>
       <p>Hello, my name is Antonio Duran</p>
     </div>
   );
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,14 +2,34 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
+    <div class="card border-light w-50">
       <h2>Contact Me</h2>
-      <h3>Name:</h3>
-      <textarea></textarea>
-      <h3>Email:</h3>
-      <textarea></textarea>
-      <h3>Message:</h3>
-      <textarea></textarea>
+      <form>
+        <div class="form-group">
+          <label for="nameInput">Name:</label>
+          <input type="name" class="form-control" id="nameInput"></input>
+        </div>
+        <div class="form-group">
+          <label for="emailInput">Email:</label>
+          <input
+            type="email"
+            class="form-control"
+            id="emailInput"
+            placeholder="name@example.com"
+          ></input>
+        </div>
+        <div class="form-group">
+          <label for="messageTextArea">Message:</label>
+          <textarea
+            class="form-control"
+            id="messageTextArea"
+            rows="3"
+          ></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
