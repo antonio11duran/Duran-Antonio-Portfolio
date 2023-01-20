6617e5841dc7cb6993b2c1cd7108050266fcf2e7
Duran11 <antonio11duran@gmail.com>
2023-01-20 08:59:21 -0700

    added content to about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h2>About Me</h2>
-      <p>Hello, my name is Antonio Duran</p>
+    <div class="card border-0 w-50 mx-auto">
+      <h2 class="text-center">About Me</h2>
+      <br></br>
+      <p>
+        My name is Antonio Duran, and I am a Full Stack Developer. I am set to
+        complete a coding boot camp in February 2023. I enjoy working in a MERN
+        Stack setting, and being a part of the full development process. I live
+        in the Denver-metro area with my wife and energetic Husky. Outside of
+        work I like to stay active in intramural soccer, volleyball, and
+        basketball leagues. I enjoy story-driven and multi-player/team-based
+        video games. My current favorites are Rocket League and Horizon. I enjoy
+        solving puzzles both for fun and professionally. When I encounter a bug
+        or issue that I haven't seen before, I like to use my resources,
+        knowledge, and problem solving skills to identify the root issue and
+        resolution. When I set my mind to it, there hasn't been an issue I
+        haven't been able to resolve! Logic and coding have come easily to me,
+        and I have really enjoyed expanding my knowledge during this boot camp.
+        I look forward to connecting, and being a part of your professional
+        network!
+      </p>
     </div>
   );
 }
