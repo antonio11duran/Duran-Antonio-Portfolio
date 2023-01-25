12067fb6a66af9f8c45cbcb671e233c4733093a7
Duran11 <antonio11duran@gmail.com>
2023-01-25 15:22:43 -0700

    added links to all projects, also added some hover elements

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,12 +13,15 @@ export default function Portfolio() {
             alt="clear lake at base of mountain"
           ></img>
           <div class="card-img-overlay">
-            <h5 class="card-title">XPLOR</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
+            <h5 class="card-title">
+              <a href="https://rarellano5280.github.io/National-Park-Finder">
+                XPLOR
+              </a>{' '}
+              <a href="https://github.com/rarellano5280/National-Park-Finder">
+                <i class="fa-brands fa-github fa-2xl"></i>
+              </a>
+            </h5>
+            <p class="card-text">REST APIs</p>
           </div>
         </div>
         <div class="card bg-dark text-white">
@@ -28,12 +31,15 @@ export default function Portfolio() {
             alt="icons of different types of food"
           ></img>
           <div class="card-img-overlay">
-            <h5 class="card-title">Raving Cravings</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
+            <h5 class="card-title">
+              <a href="https://ancient-cove-76580.herokuapp.com/">
+                Raving Cravings
+              </a>{' '}
+              <a href="https://github.com/YoungANDgreen/Team5">
+                <i class="fa-brands fa-github fa-2xl"></i>
+              </a>
+            </h5>
+            <p class="card-text">MVC</p>
           </div>
         </div>
         <div class="card bg-dark text-white">
@@ -43,12 +49,15 @@ export default function Portfolio() {
             alt="weather icons depicting numerous different types of weather"
           ></img>
           <div class="card-img-overlay">
-            <h5 class="card-title">Weather App</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
+            <h5 class="card-title">
+              <a href="https://pure-taiga-67636.herokuapp.com/">
+                Google Book Search
+              </a>{' '}
+              <a href="https://github.com/antonio11duran/book-search-engine">
+                <i class="fa-brands fa-github fa-2xl"></i>
+              </a>
+            </h5>
+            <p class="card-text">MERN</p>
           </div>
         </div>
       </div>
@@ -61,7 +70,14 @@ export default function Portfolio() {
             alt="daily and monthly planner"
           ></img>
           <div class="card-img-overlay">
-            <h5 class="card-title">Daily Planner</h5>
+            <h5 class="card-title">
+              <a href="https://antonio11duran.github.io/daily-planner/">
+                Daily Planner
+              </a>{' '}
+              <a href="https://github.com/antonio11duran/daily-planner">
+                <i class="fa-brands fa-github fa-2xl"></i>
+              </a>
+            </h5>
             <p class="card-text">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
@@ -76,7 +92,14 @@ export default function Portfolio() {
             alt="question mark over black background"
           ></img>
           <div class="card-img-overlay">
-            <h5 class="card-title">Practice Interview Quiz</h5>
+            <h5 class="card-title">
+              <a href="https://antonio11duran.github.io/Practice-Interview-Quiz/">
+                Practice Interview Quiz
+              </a>{' '}
+              <a href="https://github.com/antonio11duran/Practice-Interview-Quiz">
+                <i class="fa-brands fa-github fa-2xl"></i>
+              </a>
+            </h5>
             <p class="card-text">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
@@ -91,7 +114,14 @@ export default function Portfolio() {
             alt="hand over binary holding password between fingertips"
           ></img>
           <div class="card-img-overlay">
-            <h5 class="card-title">Password Generator</h5>
+            <h5 class="card-title">
+              <a href="https://antonio11duran.github.io/weather-app/">
+                Weather App
+              </a>{' '}
+              <a href="https://github.com/antonio11duran/weather-app">
+                <i class="fa-brands fa-github fa-2xl"></i>
+              </a>
+            </h5>
             <p class="card-text">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
